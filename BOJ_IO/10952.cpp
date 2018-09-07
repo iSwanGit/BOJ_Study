d7648811e33fde261e1faff3b687cfe0abda2970
iSwanGit <emakr518@gmail.com>
2018-09-07 21:15:25 +0900

    Solved 10952

---
+++ BOJ_IO/10952.cpp
@@ -0,0 +1,18 @@
+// A+B -5
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+
+	while (true) {
+		cin >> a >> b;
+		if (!(a+b)) {
+			break;
+		}
+		cout << a + b << endl;
+	}
+	return 0;
+}
+
