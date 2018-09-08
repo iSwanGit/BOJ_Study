867cd071964cdb62ee532c043ded9ad43f037d8b
iSwanGit <emakr518@gmail.com>
2018-09-08 10:25:04 +0900

    Solved 11021

---
+++ BOJ_IO/11021.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	int cases;
+	int a;
+	int b;
+
+	scanf_s("%d", &cases);  // scanf
+	for (size_t i = 0; i < cases; i++) {
+		scanf_s("%d %d", &a, &b);       // scanf
+		printf("Case #%d: %d\n", i+1, a + b);
+	}
+	return 0;
+}
