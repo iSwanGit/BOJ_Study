8075e5ff33328a4ee5f66cf405a112c99a8e675e
iSwanGit <emakr518@gmail.com>
2018-09-08 20:54:58 +0900

    solved 10991

---
+++ BOJ_IO/10991.cpp
@@ -0,0 +1,20 @@
+// star 16
+
+#include <iostream>
+int main(int argc, char* argv[]) {
+
+	int n;
+	std::cin >> n;
+	
+	for (size_t i = 0; i < n; i++) {
+		for (size_t k = i; k < n-1; k++) {
+			std::cout << " ";
+		}
+		for (size_t j = 0; j < i+1; j++) {
+			std::cout << "* ";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
